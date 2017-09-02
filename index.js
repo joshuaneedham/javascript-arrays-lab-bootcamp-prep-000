9a4e2ba175419948dfc31ef62a150aa24200f43c
Joshua Needham <joshuabneedham@gmail.com>
2017-09-02 01:36:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,3 +30,7 @@ function removeLastKitten() {
   var kittens2 = kittens.slice(0, kittens.length -1);
   return kittens2;
 }
+function removeFirstKitten() {
+  var kittens2 = kittens.slice(1);
+  return kittens2;
+}
