c9c9d53645072b53cbbbac90e223c404499e6f7c
Joshua Needham <joshuabneedham@gmail.com>
2017-09-02 00:55:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 
 function destructivelyAppendKitten(name) {
   var kittens = ["Milo", "Otis", "Garfield"];
-  array.push[name];
+  kittens.push[name];
   return array;
 }
