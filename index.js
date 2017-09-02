f4312220825efe402309bf88570d4ec06bd03b57
Joshua Needham <joshuabneedham@gmail.com>
2017-09-02 00:47:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
   var kittens = ["Milo", "Otis", "Garfield"]
-  var kittens2 = [name, ...kittens];
-  return kittens2;
+  kittens.push[name, ...kittens];
+  return kittens;
 }
