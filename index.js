9ec38d74ee3f9b85cabaf6ce5d00697ebcfc3987
Joshua Needham <joshuabneedham@gmail.com>
2017-09-02 01:35:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,3 +26,7 @@ function prependKitten(name) {
   var kittens2 = [name, ...kittens];
   return kittens2;
 }
+function removeLastKitten() {
+  var kittens2 = kittens.slice(0, kittens.length -1);
+  return kittens2;
+}
